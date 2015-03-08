0f05ed66103bccb6bdbec9c7f9e4617cc4fc6b6a
Nalin Natrajan <nalin.natrajan@gmail.com>
2015-03-08 17:41:00 +0800

    Initial commit
    initial commit

---
+++ SimpleAutoLayout/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SimpleAutoLayout
+//
+//  Created by Nalin Natrajan on 8/3/15.
+//  Copyright (c) 2015 Nalin Natrajan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
